bf8310110b3cd4540736570727ac16d563b4f568
Radosław Wąsala <rwasala@future-processing.com>
2018-10-08 12:48:41 +0200

    Add valdiation improvemetns and reset endpoint

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const customerList = [];
-const carList = [];
+let customerList = [];
+let carList = [];
 
 app.use(bodyParser.json());
 app.listen(3000, function () {
-    customerList.push({
-        login: "admin",
-        password: "Runforest1"
-    });
-    for (let i = 1; i <= 30; ++i) {
-        carList.push({
-            id: i,
-            carName: "Car " + i,
-            avaiable: true
-        });
-    }
-    console.log('App is running...')
+    initialize();
+    console.log('App is running...');
 });
 
 app.get('/', function (req, res) {
@@ -29,7 +19,7 @@ app.post('/customer', function (req, res) {
     const user = req.body;
     user.reservations = [];
     user.validated = false;
-    if (user.login === undefined || user.password === undefined || user.password.length < 1) {
+    if (!user.login || !user.password) {
         statusCode = 400;
     } else {
         if (customerList.filter(customer => customer.login === user.login).length !== 0) {
@@ -46,11 +36,11 @@ app.post('/login', function (req, res) {
     let statusCode = 200;
     let token = "";
     const credentials = req.body;
-    if (credentials.login === undefined || credentials.password === undefined) {
+    if (!credentials.login || !credentials.password) {
         statusCode = 400;
     } else {
         const customer = customerList.filter(customer => customer.login === credentials.login && customer.password === credentials.password)[0];
-        if (customer === undefined) {
+        if (!customer) {
             statusCode = 400;
         } else {
             token = generateRandomString();
@@ -64,12 +54,12 @@ app.post('/addCreditCard', function (req, res) {
     let statusCode = 204;
     let customer = {};
     const creditCard = req.body.creditCardNumber;
-    if (req.headers.authorization === undefined || creditCard === undefined || creditCard.length != 12) {
+    if (!req.headers.authorization|| !creditCard || creditCard.length != 12) {
         statusCode = 400;
     } else {
         const token = req.headers.authorization.replace("Bearer ", "");
         customer = customerList.filter(customer => customer.token === token)[0];
-        if (customer === undefined) {
+        if (!customer) {
             statusCode = 403;
         } else {
             customer.creditCardNumber = creditCard;
@@ -82,12 +72,12 @@ app.get('/customer', function (req, res) {
     let statusCode = 200;
     let customer = {};
     const response = {};
-    if (req.headers.authorization === undefined) {
+    if (!req.headers.authorization) {
         statusCode = 400;
     } else {
         const token = req.headers.authorization.replace("Bearer ", "");
         customer = customerList.filter(customer => customer.token === token)[0];
-        if (customer === undefined) {
+        if (!customer) {
             statusCode = 403;
         } else {
             response.login = customer.login;
@@ -103,7 +93,7 @@ app.patch('/changePassword', function (req, res) {
     let statusCode = 204;
     let customer = {};
     const newPassword = req.body.password;
-    if (req.headers.authorization === undefined || newPassword === undefined) {
+    if (!req.headers.authorization || !newPassword) {
         statusCode = 400;
     } else {
         const token = req.headers.authorization.replace("Bearer ", "");
@@ -120,12 +110,12 @@ app.patch('/changePassword', function (req, res) {
 app.patch('/validate/:login', function (req, res) {
     let statusCode = 204;
     const login = req.params.login;
-    if (req.headers.authorization === undefined) {
+    if (!req.headers.authorization) {
         statusCode = 400;
     } else {
         const token = req.headers.authorization.replace("Bearer ", "");
         customer = customerList.filter(customer => customer.token === token && customer.login === "admin")[0];
-        if (customer === undefined) {
+        if (!customer) {
             statusCode = 403;
         } else {
             customerIndex = customerList.findIndex((customer => customer.login === login));
@@ -146,19 +136,19 @@ app.get('/cars', function (req, res) {
 app.post('/createReservation', function (req, res) {
     let statusCode = 201;
     const carId = req.body.carId;
-    if (req.headers.authorization === undefined || carId === undefined || carId < 1) {
+    if (!req.headers.authorization || !carId || carId < 1) {
         statusCode = 400;
     } else {
         const token = req.headers.authorization.replace("Bearer ", "");
         const customer = customerList.filter(customer => customer.token === token)[0];
-        if (customer === undefined) {
+        if (!customer) {
             statusCode = 403;
         } else {
-            if (customer.creditCardNumber === undefined || customer.validated === false) {
+            if (!customer.creditCardNumber || customer.validated === false) {
                 statusCode = 405;
             } else {
                 const car = carList[carId - 1];
-                if (car === undefined) {
+                if (!car) {
                     statusCode = 404;
                 } else {
                     if (car.avaiable === false) {
@@ -176,6 +166,27 @@ app.post('/createReservation', function (req, res) {
     }
     res.sendStatus(statusCode);
 });
+
+app.get('/reset', function (req, res) {
+    initialize();
+    res.sendStatus(200);
+});
+
+const initialize = function () {
+    customerList = [];
+    carList = [];
+    customerList.push({
+        login: "admin",
+        password: "Runforest1"
+    });
+    for (let i = 1; i <= 30; ++i) {
+        carList.push({
+            id: i,
+            carName: "Car " + i,
+            avaiable: true
+        });
+    }
+}
 
 const generateRandomString = function () {
     let text = '';
